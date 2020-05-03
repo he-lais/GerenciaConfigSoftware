a4080ead290ad97a7bc70fc1b1a975c4c8269015
Tomas Costa <tomas.costa@localiza.com>
2020-05-02 23:40:30 -0300

    consumindo dados do firebase

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-$(document).ready( function() {
-    alert('testando js');
-});
+var firebaseConfig = {
+    apiKey: "AIzaSyBQDsy-LKD4glxFqmZ8Xl3zeTK-nb0JuNg",
+    authDomain: "receitas-web.firebaseapp.com",
+    databaseURL: "https://receitas-web.firebaseio.com",
+    projectId: "receitas-web",
+    storageBucket: "receitas-web.appspot.com",
+    messagingSenderId: "682540538905",
+    appId: "1:682540538905:web:139d4548cdc8fad4489e88"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var ref = database.ref('Receitas');
+ref.on('value', getValue, error);
+
+function getValue(data){
+    console.log(data.val());
+}
+
+function error(err){
+    console.log("Erro ao obter dados: " + err);
+}
+
